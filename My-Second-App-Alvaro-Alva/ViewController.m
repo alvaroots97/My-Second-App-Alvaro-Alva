8ed2a74a7f93ce939a7f1f8dbff24e982109e92f
Alvaro Alva <7mesino.97@gmail.com>
2020-09-12 18:19:45 -0400

    Login app done

--- My-Second-App-Alvaro-Alva/ViewController.m
+++ My-Second-App-Alvaro-Alva/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UILabel *notificationLabel;
+
 
 @end
 
@@ -17,6 +21,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    //[self.notificationLabel setText:@"This is a notification"];
+    [self.passwordTextField setSecureTextEntry:YES];
+}
+- (IBAction)logMeIn:(id)sender {
+    if([self.usernameTextField.text isEqualToString:@"alvaro"] &&
+       [self.passwordTextField.text isEqualToString:@"alva"]) {
+        [self.notificationLabel setText:@"Success!"];
+    } else {
+        [self.notificationLabel setText:@"Failed!"];
+    }
+    NSLog(@"username %@", [self.usernameTextField text]);
+    NSLog(@"password %@", [self.passwordTextField text]);
 }
 
 
